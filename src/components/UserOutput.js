53e14e81349593734b680ed5f4cafb471198a17c
Senthilkumar Kalyanasundaram <senthilkumar.cse04@gmail.com>
2021-11-17 11:48:41 +0530

    removed old files

--- src/components/UserOutput.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const UserOutput = ({ userName }) => {
-    return <p style={{ color: 'purple' }}>My user name is : <strong>{userName}</strong></p>
-}
-
-export default UserOutput;
